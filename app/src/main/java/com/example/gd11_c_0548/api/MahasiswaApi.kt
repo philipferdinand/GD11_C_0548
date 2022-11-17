0018372cc5db346b55800c9d01fd314619d1a9d8
philipferdinand <philipfs150302@gmail.com>
2022-11-17 15:14:16 +0700

    GD11_C_0548

---
+++ app/src/main/java/com/example/gd11_c_0548/api/MahasiswaApi.kt
@@ -0,0 +1,13 @@
+package com.example.gd11_c_0548.api
+
+class MahasiswaApi {
+    companion object{
+        val  BASE_URl = "https://6368bba315219b84960558d7.mockapi.io/api/"
+
+        val GET_ALL_URL = BASE_URl+ "mahasiswa/"
+        val GET_BY_ID_URL = BASE_URl+"mahasiswa/"
+        val ADD_URL = BASE_URl+"mahasiswa"
+        val UPDATE_URL = BASE_URl+"mahasiswa/"
+        val DELETE_URL = BASE_URl+"mahasiswa/"
+    }
+}
